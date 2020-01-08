a1e2ad7360abf3ce8156255552ae59b820aef23b
Raviprakash <46168102+Raviprakash-B@users.noreply.github.com>
2020-01-08 21:18:15 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int array[100],search,c,n;
+	int array[100],search,i,n;
 	printf("enter the elements in array \n");
 	scanf("%d",&n);
 	
 	printf("enter %d numbers \n",n);
-	for(c=0;c<n;c++)
+	for(i=0;i<n;i++)
 	scanf("%d",&array[c]);
 	
 	printf("enter a number to search \n");
 	scanf("%d",&search);
 	
-	for(c=0;c<n;c++)
+	for(i=0;i<n;i++)
 	{
-		if(array[c]==search) /*if required element is found*/
+		if(array[i]==search) /*if required element is found*/
 		{
-			printf("%d is present at location %d. \n",search,c+1);
+			printf("%d is present at location %d. \n",search,i+1);
 			break;
 		}
 	}
-	if(c==n)
+	if(i==n)
 	printf("%d isn't present in the array \n",search);
 	
 	return 0;
